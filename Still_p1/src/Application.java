99f788b7ff8a26e60213897c7da1334437e3fede
Tate Still <tatestill@yahoo.com>
2020-09-08 00:27:31 -0400

    Adds various encryption functions

--- Still_p1/src/Application.java
+++ Still_p1/src/Application.java
@@ -6,18 +6,66 @@ public class Application {
     {
         String inputValue;
 
+        Encrypter encryptOut = new Encrypter();
+
+        inputValue = getValue();
+
+
+
+        System.out.print("You entered ")
+    }
+
+    static String getValue()
+    {
+        String inputValue;
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter value for encryption: ");
+        inputValue = input.nextLine();
+        return inputValue;
     }
 }
 
 class Encrypter{
 
-    int digits[];
+    int digits[] = new int[4];
 
-    static int addSevenModTen(Encrypter inputInt)
+
+    static String encrypt(String input)
     {
+        Encrypter encryptOut = new Encrypter();
 
+        return input;
+    }
+
+    static int[] convertStringToArray(String stringIn)
+    {
+        int arrayOut[] = new int[4];
+        int i;
+        for(i=0 ; i < 4 ; i++)
+        {
+            arrayOut[i] = stringIn.charAt(i) - 48;
+        }
+        return arrayOut;
+
+    }
+
+
+    static int addSevenModTen(int x)
+    {
+        x = x + 7;
+        x = x % 10;
+        return x;
+    }
+
+    static Encrypter firstEncrypt(Encrypter in)
+    {
+        int i;
+        for(i=0; i < 4 ; i++)
+        {
+            in.digits[i] = addSevenModTen(in.digits[i]);
+        }
+        return in;
     }
 
 }
