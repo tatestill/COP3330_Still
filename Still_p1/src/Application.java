790640d8e8ac80e70b0946f929ffa2602b0fcf0e
Tate Still <tatestill@yahoo.com>
2020-09-07 23:58:21 -0400

    Encrypter and Application inital setup

---
+++ Still_p1/src/Application.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String args[])
+    {
+        String inputValue;
+
+        Scanner input = new Scanner(System.in);
+
+    }
+}
+
+class Encrypter{
+
+    int digits[];
+
+    static int addSevenModTen(Encrypter inputInt)
+    {
+
+    }
+
+}
