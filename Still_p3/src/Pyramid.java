669ee4304bff33f93418f3b5f87f92e073688e70
Tate Still <tatestill@yahoo.com>
2020-10-12 02:53:48 -0400

    Adds import for Math.sqrt

--- Still_p3/src/Pyramid.java
+++ Still_p3/src/Pyramid.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.sqrt;
+
 public class Pyramid extends Shape3D{
     double length;
     double width;
@@ -17,8 +19,8 @@ public class Pyramid extends Shape3D{
     {
         double temp1, temp2, temp3;
         temp1 = length * width;
-        temp2 = (Math.sqrt(((width/2)*(width/2)) + (height*height))) * length;
-        temp3 = (Math.sqrt(((length/2)*(length/2)) + (height*height))) * width;
+        temp2 = sqrt((width/2) *(width/2) + height*height) * length;
+        temp3 = sqrt((length/2) *(length/2) + height*height) * width;
         return temp1 + temp2 + temp3;
     }
 
