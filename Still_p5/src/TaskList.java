586f01324248e9d2604914d655d17788344e834d
Tate Still <tatestill@yahoo.com>
2020-12-01 01:46:35 -0500

    Initial Commits

---
+++ Still_p5/src/TaskList.java
@@ -0,0 +1,160 @@
+import java.nio.file.FileAlreadyExistsException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+
+public class TaskList{
+    ArrayList<TaskItem> List = new ArrayList<TaskItem>();
+
+    public int addTask(String newTitle, String newDesc, String newDate)//Add task method returns -1 or -2 if unsuccessful, 1 if successful
+    {
+        if(!TaskItem.isTitleValid(newTitle))
+            return -1;
+        if(!TaskItem.isDueDateValid(newDate))
+            return -2;
+        TaskItem newTask = new TaskItem(newTitle, newDesc, newDate);
+        this.List.add(newTask);
+        return 1;
+
+    }
+
+    public int editTask(int index, String newTitle, String newDesc, String newDate)
+    {
+        try
+        {
+            if(!TaskItem.isTitleValid(newTitle))
+            {
+                return -1;
+            }
+            if(!TaskItem.isDueDateValid(newDate))
+            {
+                return -2;
+            }
+            TaskItem newTask = new TaskItem(newTitle, newDesc, newDate);
+            this.List.set(index, newTask);
+        }
+        catch(IndexOutOfBoundsException e)
+        {
+            return 0;
+        }
+        return 1;
+    }
+
+    public int removeTask(int index) //remove task returns 1 if removed successfully 0 if not
+    {
+        try
+        {
+            this.List.remove(index);
+        }
+        catch(IndexOutOfBoundsException e)
+        {
+            return 0;
+        }
+        return 1;
+    }
+
+    public String getTaskTitle(int index) //Returns a blank string should the index be wrong.
+    {
+        try
+        {
+            return this.List.get(index).getTitle();
+        }
+        catch(IndexOutOfBoundsException e)
+        {
+            return "";
+        }
+    }
+
+    public String getTaskDesc(int index) //Cannot return a blank string as desc can be blank, so a determined fail string that is unlikely to be a description will be used, "~~~"
+    {
+        try
+        {
+            return this.List.get(index).getDesc();
+        }
+        catch(IndexOutOfBoundsException e)
+        {
+            return "~~~";
+        }
+    }
+
+    public String getTaskDueDate(int index) //Cannot return a blank string if invalid index
+    {
+        try
+        {
+            return this.List.get(index).getDueDate();
+        }
+        catch(IndexOutOfBoundsException e)
+        {
+            return "";
+        }
+    }
+
+    public int setCompletedAtIndex(int index, boolean complete) //Returns 1 if successful, 0 if fails
+    {
+        try
+        {
+            this.List.get(index).markCompletion(complete);
+            return 1;
+        }
+        catch(IndexOutOfBoundsException e)
+        {
+            return 0;
+        }
+    }
+
+    public int saveList( String name)
+    {
+        FileOutputStream out = null;
+        ObjectOutputStream objectOut;
+        int i;
+        try
+        {
+            out = new FileOutputStream(name);
+            objectOut = new ObjectOutputStream(out);
+            objectOut.writeObject(List);
+            objectOut.close();
+            out.close();
+            return 1;
+            //for(i=0; i < this.List.size() ; i++)
+            //{
+            //out.write(i + ") ["+ this.List.get(i).getDueDate() + "]" + this.List.get(i).getTitle() + ": " + this.List.get(i).getDesc());
+
+            // }
+        }
+
+        catch (IOException e)
+        {
+            return 0;
+        }
+    }
+
+    public int loadList(String name)
+    {
+        FileInputStream in = null;
+        ObjectInputStream objectIn = null;
+        int i;
+        try
+        {
+            in = new FileInputStream(name);
+            objectIn = new ObjectInputStream(in);
+            List = (ArrayList<TaskItem>) objectIn.readObject();
+
+            return 1;
+        }
+        catch(FileNotFoundException e)
+        {
+
+            return 0;
+        }
+        catch(IOException e)
+        {
+            System.out.print(e.getMessage());
+            return -1;
+        }
+        catch (ClassNotFoundException e)
+        {
+
+            return -2;
+        }
+    }
+}
---
+++ Still_p5/src/TaskListTest.java
@@ -0,0 +1,186 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TaskListTest {
+
+
+    @Test
+    public void newTaskListIsEmpty()
+    {
+        TaskList testList = new TaskList();
+        assertEquals(0, testList.List.size());
+    }
+
+    @Test
+    public void addingValidTaskItemsIncreasesSize()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Title", "Desc", "2020-12-31");
+        testList.addTask("Title1", "Desc1", "2021-01-01");
+        assertEquals(2, testList.List.size());
+    }
+
+    @Test
+    public void addingInvalidTitleTaskDoesNotIncreaseSize()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Title", "Desc", "2020-12-31");
+        testList.addTask("", "Desc1", "2021-01-01");
+        assertEquals(1, testList.List.size());
+    }
+
+    @Test
+    public void addingInvalidDueDateTaskDoesNotIncreaseSize()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Title", "Desc", "2020-12-31");
+        testList.addTask("Title1", "Desc1", "");
+        assertEquals(1, testList.List.size());
+    }
+
+    @Test
+    public void editingTaskTitleChangesValue()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Title", "Desc", "2020-12-31");
+        testList.editTask(0,"newTitle", "Desc", "2020-12-31");
+        assertEquals("newTitle", testList.List.get(0).getTitle());
+    }
+
+    @Test
+    public void editingTaskDescChangesValue()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Title", "Desc", "2020-12-31");
+        testList.editTask(0,"Title", "newDesc", "2020-12-31");
+        assertEquals("newDesc", testList.List.get(0).getDesc());
+    }
+
+    @Test
+    public void editingTaskDueDateChangesValue()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Title", "Desc", "2020-12-31");
+        testList.editTask(0,"Title", "Desc", "2021-11-22");
+        assertEquals("2021-11-22", testList.List.get(0).getDueDate());
+    }
+
+    @Test
+    public void editingTaskInvalidIndexFails()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Title", "Desc", "2020-12-31");
+        assertEquals(0, testList.editTask(2,"Title", "Desc", "2021-11-22"));
+    }
+
+    @Test
+    public void removingTaskLowersSize()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Title", "Desc", "2020-12-31");
+        testList.addTask("Title", "Desc", "2020-12-31");
+        testList.removeTask(0);
+        assertEquals(1, testList.List.size());
+
+    }
+
+    @Test
+    public void removingTaskInvalidIndexFails()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Title", "Desc", "2020-12-31");
+        testList.addTask("Title", "Desc", "2020-12-31");
+
+        assertEquals(0, testList.removeTask(4));
+
+    }
+
+    @Test
+    public void getTitleOfTaskAtValidIndex()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Not this one", "Not the one being aimed for", "2020-12-31");
+        testList.addTask("This one", "The one being aimed for", "2020-12-31");
+        testList.addTask("Not this one", "Not the one being aimed for", "2020-12-31");
+        assertEquals("This one", testList.getTaskTitle(1));
+    }
+
+    @Test
+    public void failIfGettingTitleOfTaskAtInvalidIndex()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Title", "Desc", "2020-12-31");
+        testList.addTask("Title", "Desc", "2020-12-31");
+        assertEquals("", testList.getTaskTitle(3));
+    }
+
+    @Test
+    public void getDescOfTaskAtValidIndex()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Not this one", "Not the one being aimed for", "2020-12-31");
+        testList.addTask("This one", "The one being aimed for", "2020-12-31");
+        testList.addTask("Not this one", "Not the one being aimed for", "2020-12-31");
+        assertEquals("The one being aimed for", testList.getTaskDesc(1));
+    }
+
+    @Test
+    public void failIfGettingDescOfTaskAtInvalidIndex()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Title", "Desc", "2020-12-31");
+        testList.addTask("Title", "Desc", "2020-12-31");
+        assertEquals("~~~", testList.getTaskDesc(3));
+    }
+
+    @Test
+    public void getDueDateOfTaskAtValidIndex()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Not this one", "Not the one being aimed for", "2020-12-31");
+        testList.addTask("This one", "The one being aimed for", "2021-09-22");
+        testList.addTask("Not this one", "Not the one being aimed for", "2020-12-31");
+        assertEquals("2021-09-22", testList.getTaskDueDate(1));
+    }
+
+    @Test
+    public void failIfGettingDueDateOfTaskAtInvalidIndex()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Title", "Desc", "2020-12-31");
+        testList.addTask("Title", "Desc", "2020-12-31");
+        assertEquals("", testList.getTaskDueDate(3));
+    }
+
+    @Test
+    public void SetCompletedTaskAtValidIndex()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Title", "Desc", "2020-12-31");
+        testList.setCompletedAtIndex(0, true);
+        assertTrue(testList.List.get(0).isCompleted());
+    }
+
+    @Test
+    public void FailsToSetCompletedTaskAtInvalidIndex()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Title", "Desc", "2020-12-31");
+        assertEquals(0, testList.setCompletedAtIndex(3, true));
+    }
+
+    @Test
+    public void ListLoadsFromSave()
+    {
+        TaskList testList = new TaskList();
+        testList.addTask("Title", "Desc", "2020-12-31");
+        testList.saveList("List.txt");
+        TaskList newTestList = new TaskList();
+        newTestList.loadList("List.txt");
+        assertEquals(testList.List.get(0).getTitle(), newTestList.List.get(0).getTitle());
+    }
+
+}
+
